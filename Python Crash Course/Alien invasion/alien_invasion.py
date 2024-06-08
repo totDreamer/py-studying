f70ff2bd48db569ea670b5b882fec254f0546840
totDreamer- <brumaldreamer@gmail.com>
2024-06-08 12:50:12 +0500

    Made a ship moving in all directions with screen restrictions

--- Python Crash Course/Alien invasion/alien_invasion.py
+++ Python Crash Course/Alien invasion/alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+
 
 class AlienInvasion:
     """Class for managing game resources and behavior"""
@@ -10,20 +12,56 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.ship = Ship(self)
         pygame.display.set_caption("Alien Invasion")
 
     def run_game(self):
         """Run the game"""
         while True:
-            """Keyboard and mouse event tracking"""
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
 
-            """Each time the loop passes, the screen is redrawn"""
-            self.screen.fill(self.settings.bg_color)
-            """Display the last screen drawn"""
-            pygame.display.flip()
+    def _check_events(self):
+        """Handles keystrokes and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _update_screen(self):
+        """Each time the loop passes, the screen is redrawn"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Display the last screen drawn
+        pygame.display.flip()
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            # Move ship right
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
+
 
 if __name__ == '__main__':
     ai = AlienInvasion()
--- Python Crash Course/Alien invasion/settings.py
+++ Python Crash Course/Alien invasion/settings.py
@@ -1,5 +1,6 @@
 class Settings:
     def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.screen_width = 800
+        self.screen_height = 600
+        self.ship_speed = 0.2
+        self.bg_color = (25, 25, 112)
